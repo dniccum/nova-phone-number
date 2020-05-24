f321551119481062098f125196c3a22cc6f2d525
lintaba <lintaba@gmail.com>
2020-05-24 18:55:40 +0200

    Update DetailField.vue

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -6,7 +6,7 @@
 export default {
     props: ['resource', 'resourceName', 'resourceId', 'field'],
     beforeMount() {
-        if (this.field.linkOnDetail) {
+        if (this.field.linkOnDetail && this.field.value) {
             this.field.asHtml = true;
             this.field.value = `<a href="tel:${this.field.value}" class="text-primary">${this.field.value}</a>`
         }
