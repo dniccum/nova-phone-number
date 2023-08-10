1518fc3ef0aca0e2b4b24ec638a785845c09d813
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-08-10 17:08:01 +0100

    Allow link to be clicked on Index

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,5 +1,10 @@
 <template>
+  <div v-if="this.field.linkOnIndex" @click.stop>
     <span v-html="field.value"></span>
+  </div>
+  <div v-else>
+    <span v-html="field.value"></span>
+  </div>
 </template>
 
 <script>
@@ -7,7 +12,7 @@ export default {
     props: ['resourceName', 'field'],
     beforeMount() {
         if (this.field.linkOnIndex) {
-            this.field.value = `<a href="tel:${this.field.value}" class="link-default">${this.field.value}</a>`
+          this.field.value = `<a href="tel:${this.field.value}" class="link-default">${this.field.value}</a>`;
         }
     }
 }
